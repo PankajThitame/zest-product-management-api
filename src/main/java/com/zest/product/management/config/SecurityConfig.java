aee4dc1f00884060668970c6538d1de5e57394b9
unknown <thitamepankaj@gmail.com>
2026-02-20 03:46:55 +0530

    Cleanup: remove unused Environment import in SecurityConfig

--- src/main/java/com/zest/product/management/config/SecurityConfig.java
+++ src/main/java/com/zest/product/management/config/SecurityConfig.java
@@ -5,7 +5,6 @@ import com.zest.product.management.security.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
